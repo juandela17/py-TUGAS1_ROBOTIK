7d77116e3bd4eb5d6a6b9077214c1fa568945517
juandela17 <47452930+juandela17@users.noreply.github.com>
2020-03-21 20:50:25 +0700

    Add files via upload

---
+++ TUGAS1.py
@@ -0,0 +1,11 @@
+print(" PROGRAM PENJUMLAHAN DUA BILANGAN DENGAN MEMINTA INPUT DARI USER ")
+print(" CREATED BY JUANDELA HERINA PUTRI ")
+
+bil1 = input('Masukkan bilangan pertama: ') 
+bil2 = input('Masukkan bilangan kedua: ') 
+
+# Menjumlahkan bilangan 
+jumlah = float(bil1) + float(bil2) 
+
+# Menampilkan jumlah 
+print('Jumlah {0} + {1} adalah {2}'.format(bil1, bil2, jumlah))
